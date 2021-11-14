f1baf5c72e96bc0beb3a24727f13614b516fdf42
JArvis@ <40257199+utpal434@users.noreply.github.com>
2021-11-14 20:29:21 +0530

    msg

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -15,7 +15,7 @@ public class OrderDao {
                 new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
-                new ORDER(100,"Keyboard",2,2000)
+                
                 .collect(Collectors.toList());
     }
 }
