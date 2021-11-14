b220878c0b12f38b8c1d05aa6f547ff464075b77
JArvis@ <40257199+utpal434@users.noreply.github.com>
2021-11-14 20:21:12 +0530

    msg

--- src/main/java/com/javatechie/aws/cicd/example/Order.java
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -15,38 +15,5 @@ public class Order {
     private String name;
     private int quantity;
     private long price;
-	public Order(int id, String name, int quantity, long price) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.quantity = quantity;
-		this.price = price;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	public long getPrice() {
-		return price;
-	}
-	public void setPrice(long price) {
-		this.price = price;
-	}
-    
-    
-    
+	
 }
