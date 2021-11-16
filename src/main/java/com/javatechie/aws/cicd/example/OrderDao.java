9c1fe6fdd71d3be70abe7f9edf6bb3bda591e0ab
JArvis@ <40257199+utpal434@users.noreply.github.com>
2021-11-16 11:53:48 +0530

    fixed dao error

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -14,8 +14,7 @@ public class OrderDao {
         return Stream.of(
                 new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
-                new Order(205, "Laptop", 1, 150000),
-                
+                new Order(205, "Laptop", 1, 150000)
                 .collect(Collectors.toList());
     }
 }
