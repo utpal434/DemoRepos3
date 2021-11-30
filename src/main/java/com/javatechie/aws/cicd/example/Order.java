cbe0e68c898700ddd08ceeb6004d53e2916f31be
JArvis@ <40257199+utpal434@users.noreply.github.com>
2021-11-30 11:39:27 +0530

    testing push action

--- src/main/java/com/javatechie/aws/cicd/example/Order.java
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -5,15 +5,43 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+
 public class Order {
 
-    private int id;
+    public Order(int id, String name, int quantity, long price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	private int id;
     private String name;
     private int quantity;
     private long price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
 	
 }
